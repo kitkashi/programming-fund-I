011c6e9a07f602742ad038177aac8e4d5731754f
Sarah Kit Edwards <skit.edwards@gmail.com>
2024-09-24 17:48:14 -0700

    Add files via upload

---
+++ dogyears.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+
+{
+    int age;
+    string dogName;
+    
+    cout << "What is your dogs age? ";
+    cin >> age;
+    cin.ignore();//ignore the\n in the keybord buffer
+    cout<< "What is your dogs name? ";
+    getline(cin,dogName);
+    int dogYears= age *7;//make sure to put calculations after input
+    cout << dogName << ", you are " << dogYears << ", years old!";
+    return 0;
+
+}
